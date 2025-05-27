91ff518d33cf3b78881c3c7d0aced6842368cbcb
Shourya1224 <shourya21m@gmail.com>
2025-05-27 23:32:38 +0530

    hello

---
+++ PalindromeString.java
@@ -0,0 +1,23 @@
+public class PalindromeString {
+    public static boolean isPalindrome(String s) {
+        s = s.replaceAll("\\s", "").toLowerCase(); 
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String input = "A man a plan a canal Panama";
+        if (isPalindrome(input)) {
+            System.out.println("\"" + input + "\" is a palindrome.");
+        } else {
+            System.out.println("\"" + input + "\" is not a palindrome.");
+        }
+    }
+}
